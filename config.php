8e798f5296dba865b0f9c6b68bd116db7bd52f6b
st0907 <tp077056@mail.apu.edu.my>
2025-08-08 17:46:26 +0800

    Update config.php

--- config.php
+++ config.php
@@ -4,7 +4,7 @@ Program Name: config.php
 Description: Link the php to the database sim
 First Written on: Wednesday, 18-June-2025
 */
-$con = new mysqli("localhost", "root", "", "sim2");
+$con = new mysqli("localhost", "root", "", "sim");
 
 if ($con->connect_error) {
     die("Failed to connect to database: " . $con->connect_error);
